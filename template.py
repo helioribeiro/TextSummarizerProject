3a658324c421a0238dc403974c4050cbceaf0a09
Helio Ribeiro <helioribeiropro@gmail.com>
2023-08-22 20:21:03 -0300

    Major updates, final commit before deploy

--- template.py
+++ template.py
@@ -4,20 +4,21 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
+
 project_name = "textSummarizer"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
-    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/conponents/__init__.py",
     f"src/{project_name}/utils/__init__.py",
     f"src/{project_name}/utils/common.py",
-    f"src/{project_name}/logging//__init__.py",
+    f"src/{project_name}/logging/__init__.py",
     f"src/{project_name}/config/__init__.py",
     f"src/{project_name}/config/configuration.py",
-    f"src/{project_name}/pipeline/configuration.py",
-    f"src/{project_name}/entity/configuration.py",
-    f"src/{project_name}/constants/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
     "params.yaml",
     "app.py",
@@ -25,8 +26,10 @@ list_of_files = [
     "Dockerfile",
     "requirements.txt",
     "setup.py",
-    "research/trials.ipynb"
+    "research/trials.ipynb",
+
 ]
+
 
 for filepath in list_of_files:
     filepath = Path(filepath)
@@ -36,10 +39,14 @@ for filepath in list_of_files:
         os.makedirs(filedir, exist_ok=True)
         logging.info(f"Creating directory:{filedir} for the file {filename}")
 
-    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) ==0):
+    
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath,'w') as f:
             pass
-            logging.info(f"Creating empty file:{filepath}")
+            logging.info(f"Creating empty file: {filepath}")
 
+
+    
     else:
-        logging.info(f"{filename} already exists.")
+        logging.info(f"{filename} is already exists")
+
